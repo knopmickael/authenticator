452fb89a5c29084b44742c8a5da20004fc9cc678
Mickael Knop <knopmickael@gmail.com>
2023-01-11 17:22:33 -0300

    login service upd

--- src/usecases/login.ts
+++ src/usecases/login.ts
@@ -1,5 +1,53 @@
-export default class Login {
-  exec() {
-    return "foobar";
+import { SessionRepository } from "../../infra/repositories/in-memory/session-repository";
+import { TokenRepository } from "../../infra/repositories/in-memory/token-repository";
+import { UserRepository } from "../../infra/repositories/in-memory/user-repository";
+
+type ExpectedBody = {
+  username: string,
+  password: string
+};
+
+export class Login {
+  constructor(
+    protected userRepo: UserRepository,
+    protected tokenRepo: TokenRepository,
+    protected sessionRepo: SessionRepository
+  ) {}
+
+  protected async validateBody(body: ExpectedBody): Promise<any | boolean> {
+    // validation goes here...
+    return body;
+  }
+
+  public async exec(body: ExpectedBody): Promise<any | boolean> {
+
+    const validatedBody = await this.validateBody(body);
+    if (!validatedBody) return false;
+
+    let foundUser;
+    try {
+      foundUser = await this.userRepo.findByFieldValue(validatedBody.username);
+    } catch (error) {
+      throw new Error("Error while searching the user.");
+    }
+
+    let foundToken;
+    try {
+      foundToken = await this.tokenRepo.findByFieldValue(foundUser.getId());
+    } catch (error) {
+      throw new Error("Error while searching this user token.");
+    }
+
+    let foundSession;
+    try {
+      foundSession = await this.sessionRepo.findByFieldValue(foundToken.getId());
+    } catch (error) {
+      throw new Error("Error while searching this user session.");
+    }
+
+    return {
+      token: foundToken.getId(),
+      session: foundSession.getId()
+    }
   }
 }
--- test/usecases/login.spec.ts
+++ test/usecases/login.spec.ts
@@ -1,8 +1,30 @@
-import Login from "../../src/usecases/login";
+import { SessionRepository } from "../../infra/repositories/in-memory/session-repository";
+import { TokenRepository } from "../../infra/repositories/in-memory/token-repository";
+import { UserRepository } from "../../infra/repositories/in-memory/user-repository";
+import Session from "../../src/entities/session";
+import Token from "../../src/entities/token";
+import User from "../../src/entities/user";
+import { Login as SUT } from "../../src/usecases/login";
 
 describe('Name of the group', () => {
-  it('should return foobar', () => {
-    const login = new Login();
-    expect(login.exec()).toBe('foobar');
+  it('should login', async () => {
+    
+    const userInMemoryRepo = new UserRepository();
+    const tokenInMemoryRepo = new TokenRepository();
+    const sessionInMemoryRepo = new SessionRepository();
+
+    const login = new SUT(
+      userInMemoryRepo,
+      tokenInMemoryRepo,
+      sessionInMemoryRepo
+    );
+
+    const res = await login.exec({
+      username: "test-username",
+      password: "test-password"
+    });
+
+    expect(res).toHaveProperty('token');
+    expect(res).toHaveProperty('session');
   });
 });
