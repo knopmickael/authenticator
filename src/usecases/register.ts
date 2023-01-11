b512e0a5ae59452c0170e3f2e75294825aa4c557
Mickael Knop <knopmickael@gmail.com>
2023-01-11 12:53:53 -0300

    register service static up

--- src/usecases/register.ts
+++ src/usecases/register.ts
@@ -1,5 +1,84 @@
-export default class Register {
-  exec() {
-    return "foobar";
+import { SessionRepository } from "../../infra/repositories/in-memory/session-repository";
+import { TokenRepository } from "../../infra/repositories/in-memory/token-repository";
+import { UserRepository } from "../../infra/repositories/in-memory/user-repository";
+import Session from "../entities/session";
+import Token from "../entities/token";
+import User from "../entities/user";
+
+type ExpectedBody = {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+};
+
+export class Register {
+  constructor(
+    protected userRepo: UserRepository,
+    protected tokenRepo: TokenRepository,
+    protected sessionRepo: SessionRepository
+  ) {}
+
+  protected generateModels(body: ExpectedBody) {
+
+    let user: User;
+    try {
+      user = User.build(body);
+    } catch (error) {
+      throw new Error("Error while creating the user.");
+    }
+    if (!user)
+      throw new Error("User not created.");
+
+    let token: Token;
+    try {
+      token = Token.build({
+        user_id: user.getId(),
+        token: "foobar", // use jwt token instead...
+      });
+    } catch (error) {
+      throw new Error("Error while creating the token.");
+    }
+    if (!token)
+      throw new Error("Token not created.");
+
+    let session: Session;
+    try {
+      session = Session.build({
+        user_id: user.getId(),
+        token_id: token.getId()
+      });
+    } catch (error) {
+      throw new Error("Error while creating the session.");
+    }
+    if (!session)
+      throw new Error("Session not created.");
+
+    return {
+      user: user,
+      token: token,
+      session: session
+    };
+  }
+  
+  protected persistModels() {
+    return true;
+  }
+
+  public exec(body: ExpectedBody): object | boolean {
+
+    // validate body here...
+
+    const {user, token, session} = this.generateModels(body);
+
+    if (1!=1) return false;
+
+    return {
+      name: user.getProps().name,
+      email: user.getProps().email,
+      username: user.getProps().username,
+      token: token.getProps().token,
+      session: session.getId()
+    };
   }
 }
--- test/usecases/register.spec.ts
+++ test/usecases/register.spec.ts
@@ -1,8 +1,41 @@
-import Register from "../../src/usecases/register";
+import { SessionRepository } from "../../infra/repositories/in-memory/session-repository";
+import { TokenRepository } from "../../infra/repositories/in-memory/token-repository";
+import { UserRepository } from "../../infra/repositories/in-memory/user-repository";
+import Session from "../../src/entities/session";
+import Token from "../../src/entities/token";
+import User from "../../src/entities/user";
+import { Register } from "../../src/usecases/register";
 
-describe('Name of the group', () => {
-  it('should return foobar', () => {
-    const register = new Register();
-    expect(register.exec()).toBe('foobar');
+describe("Register service test suite", () => {
+  it("should create the user", () => {
+
+    const register = new Register(
+      new UserRepository(),
+      new TokenRepository(),
+      new SessionRepository()
+    );
+
+    let res;
+    try {
+      res = register.exec({
+        name: "Test name",
+        email: "test@mail.com",
+        username: "test-username",
+        password: "test-password",
+      });
+    } catch (error) {
+      throw new Error("Error while creating the user.");
+    }
+
+    // console.log(res);
+    // expect(res).toBeInstanceOf(User);
+    // expect(res).toBeInstanceOf(Token);
+    // expect(res).toBeInstanceOf(Session);
+
+    expect(res).toHaveProperty('name');
+    expect(res).toHaveProperty('email');
+    expect(res).toHaveProperty('username');
+    expect(res).toHaveProperty('token');
+    expect(res).toHaveProperty('session');
   });
 });
