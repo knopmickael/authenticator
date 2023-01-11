818f352870d0c6574a6d30005e1a6ce26aacf83e
Mickael Knop <knopmickael@gmail.com>
2023-01-11 17:22:17 -0300

    entities upd

--- src/entities/session.ts
+++ src/entities/session.ts
@@ -3,6 +3,7 @@ import { Entity } from "./_entity";
 type SessionProps = {
   user_id: string,
   token_id: string
+  expires_in?: Date
 }
 
 export default class Session extends Entity<SessionProps> {
@@ -11,6 +12,13 @@ export default class Session extends Entity<SessionProps> {
   }
 
   public static build(props: SessionProps, id?: string) {
-    return new Session(props, id);
+    
+    let date = new Date();
+    date.setDate(date.getDate() + 1);
+
+    return new  Session({
+      ...props,
+      expires_in: props.expires_in || date // 24 hours
+    }, id);
   }
 }
--- src/entities/token.ts
+++ src/entities/token.ts
@@ -3,7 +3,6 @@ import { Entity } from "./_entity";
 type TokenProps = {
   user_id: string,
   token: string,
-  expires_in?: Date
 }
 
 export default class Token extends Entity<TokenProps> {
@@ -12,13 +11,6 @@ export default class Token extends Entity<TokenProps> {
   }
 
   public static build(props: TokenProps, id?: string) {
-    
-    let date = new Date();
-    date.setDate(date.getDate() + 1);
-
-    return new Token({
-      ...props,
-      expires_in: props.expires_in || date // 24 hours
-    }, id);
+    return new Token(props, id);
   }
 }
